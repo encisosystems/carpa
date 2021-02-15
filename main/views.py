b7b45689025a5bc029d68cf5bc707ce7a57aa570
dpalacios <diferpaho@gmail.com>
2021-02-15 15:45:17 -0500

    added first settings and template files

--- main/views.py
+++ main/views.py
@@ -2,6 +2,10 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 
+
 def home(request):
     return HttpResponse('<h1>It worked!</h1>'
                         '<div>Made by <a href="https://www.encisosystems.com" target="_blank">EncisoSystems</a></div>')
+
+def login(request):   
+    return render(request, "login.html")
