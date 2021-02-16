c132d1bbb1f226ea2937b57a0c7e3e5c89e62d6f
diferpaho <46627032+diferpaho@users.noreply.github.com>
2021-02-16 08:56:49 -0500

    add home and its setup (#17)

--- main/views.py
+++ main/views.py
@@ -4,8 +4,7 @@ from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse('<h1>It worked!</h1>'
-                        '<div>Made by <a href="https://www.encisosystems.com" target="_blank">EncisoSystems</a></div>')
+    return render(request, "home.html")
 
 def login(request):   
     return render(request, "login.html")
