fc9fa909b0f34d091c699fac3ffb68328ccab5a4
dpalacios <diferpaho@gmail.com>
2021-02-16 09:34:28 -0500

    git add menu reports and its setup

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,6 @@
 # general imports
 from django.urls import path
-from main.views import home, login, people, vehicles, origin
+from main.views import home, login, people, vehicles, reports
 
 # api imports
 
@@ -13,4 +13,5 @@ urlpatterns = [
     path('people', people, name="people"),
     path('vehicles', vehicles, name="vehicles"),
     path('origin', origin, name="origin"),
+    path('reports', reports, name="reports"),
 ]
