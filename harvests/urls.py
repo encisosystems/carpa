017562d4cfd63621413ca516080ba7e317d17310
dpalacios <diferpaho@gmail.com>
2021-02-17 10:19:23 -0500

    harvests changes added

--- harvests/urls.py
+++ harvests/urls.py
@@ -1,6 +1,6 @@
 # general imports
 from django.urls import path
-from harvests.views import people, vehicles, origin
+from harvests.views import people, vehicles, origin, harvests
 
 # api imports
 
@@ -8,6 +8,7 @@ from harvests.views import people, vehicles, origin
 
 # general urls
 urlpatterns = [
+    path('harvests', harvests, name="harvests"),
     path('people', people, name="people"),
     path('vehicles', vehicles, name="vehicles"),
     path('origin', origin, name="origin"),
--- harvests/views.py
+++ harvests/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def harvests(request):   
+    return render(request, "harvests.html")
+
 def people(request):   
     return render(request, "people.html")
 
