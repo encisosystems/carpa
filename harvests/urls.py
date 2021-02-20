b2f1f215278af2a941a393d2c1744ad94a3c33d1
osbbarr1 <osbbarr1@gmail.com>
2021-02-20 11:33:59 -0500

    Add cards for bunch batch, bunch, sensor and bunch category

--- harvests/urls.py
+++ harvests/urls.py
@@ -1,6 +1,6 @@
 # general imports
 from django.urls import path
-from harvests.views import people, vehicles, origin, harvests
+from harvests.views import people, vehicles, origin, harvests,bunchbatch,bunch,sensor,bunchcategory
 
 # api imports
 
@@ -12,4 +12,9 @@ urlpatterns = [
     path('people', people, name="people"),
     path('vehicles', vehicles, name="vehicles"),
     path('origin', origin, name="origin"),
+    path('bunchbatch', bunchbatch, name="bunchbatch"),
+    path('bunch', bunch, name="bunch"),
+    path('sensor', sensor, name="sensor"),
+    path('bunchcategory', bunchcategory, name="bunchcategory"),
+    
 ]
--- harvests/views.py
+++ harvests/views.py
@@ -12,4 +12,15 @@ def vehicles(request):
 
 def origin(request):   
     return render(request, "origin.html")
-    
+
+def bunchbatch(request):   
+    return render(request, "bunchbatch.html")    
+
+def bunch(request):   
+    return render(request, "bunch.html")  
+
+def sensor(request):   
+    return render(request, "sensor.html")    
+
+def bunchcategory(request):   
+    return render(request, "bunchcategory.html")      
