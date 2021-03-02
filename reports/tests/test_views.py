752c51730400aaf30ceca04eca855f0e6d285fdc
juan5d <juanzm27@gmail.com>
2021-03-02 08:33:39 -0500

    Add test_query_report

--- reports/tests/test_views.py
+++ reports/tests/test_views.py
@@ -4,6 +4,9 @@ from django.urls import reverse
 
 class TestViews(TestCase):
 
-
-
-
+    def test_query_report_GET(self):
+        client = Client()
+        response = client.get(reverse('query_report'))
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'query_report.html')
+ 
