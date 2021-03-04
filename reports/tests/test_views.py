f51138095b202146cb8c055a9af76394dbd52707
ElkinMoreno01226 <ingelkinmh@gmail.com>
2021-03-04 16:58:27 -0500

    add test create report in test_views.py

--- reports/tests/test_views.py
+++ reports/tests/test_views.py
@@ -9,4 +9,10 @@ class TestViews(TestCase):
         response = client.get(reverse('query_report'))
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'query_report.html')
+
+    def test_create_report_GET(self):
+        client = Client()
+        response = client.get(reverse('create_report'))
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'create_report.html')
  
