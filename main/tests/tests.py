cb8fd2ad23420bc2dcb128aaa9320882e1b5e3ed
Jorge Serrano <jaserranoj@unal.edu.co>
2021-03-05 18:52:17 -0500

    Feature: Create interface test for login #159

--- main/tests/tests.py
+++ main/tests/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.http import HttpRequest
+from django.test import SimpleTestCase
+from django.urls import reverse
+
+from . import views
+
 
 # Create your tests here.
+
+class LoginPageTests(SimpleTestCase):
+
+    def test_login_page_status_code(self):
+        response = self.client.get('/')
+        self.assertEquals(response.status_code, 200)
+
+    def test_login_page_contains_correct_html(self):
+        response = self.client.get('/')
+        self.assertContains(response, '<form class="user">')
