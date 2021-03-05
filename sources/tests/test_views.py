054497e5218a35bf30c0ebc017837723b40b2160
Pedro_N <pcarodi@outllook.com>
2021-03-04 22:55:18 -0500

    Prueba no exitosa de response de people problemas docker pero el codigo del test unitario es el correcto

--- sources/tests/test_views.py
+++ sources/tests/test_views.py
@@ -1,5 +1,6 @@
 from django.test import TestCase, Client
 from django.urls import reverse
+from harvests.urls import people, harvests
 
 
 class TestViews(TestCase):
@@ -16,4 +17,8 @@ class TestViews(TestCase):
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'origin.html')
 
-
+    def test_people_GET(self):
+        client = Client()
+        response = client.get(reverse('people'))
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'people.html')
