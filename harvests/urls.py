3fbe4a6452916105e9ec6809bbde38eff5f3404f
osbbarr1 <osbbarr1@gmail.com>
2021-03-04 19:01:32 -0500

    I check and correct everything

--- harvests/urls.py
+++ harvests/urls.py
@@ -1,6 +1,6 @@
 # general imports
 from django.urls import path
-from harvests.views import people, harvests
+from harvests.views import people, harvests,bunchbatch,bunch,bunchcategory
 
 # api imports
 
@@ -10,4 +10,7 @@ from harvests.views import people, harvests
 urlpatterns = [
     path('harvests', harvests, name="harvests"),
     path('people', people, name="people"),
+    path('bunchbatch', bunchbatch, name="bunchbatch"),
+    path('bunch', bunch, name="bunch"),
+    path('bunchcategory', bunchcategory, name="bunchcategory"),
 ]
--- sources/urls.py
+++ sources/urls.py
@@ -1,6 +1,6 @@
 # general imports
 from django.urls import path
-from sources.views import vehicles, parcel,bunchbatch, bunch,sensor,bunchcategory
+from sources.views import vehicles, parcel,sensor
 
 # api imports
 
@@ -10,8 +10,5 @@ from sources.views import vehicles, parcel,bunchbatch, bunch,sensor,bunchcategor
 urlpatterns = [
     path('vehicles', vehicles, name="vehicles"),
     path('parcel', origin, name="parcel"),
-    path('bunchbatch', bunchbatch, name="bunchbatch"),
-    path('bunch', bunch, name="bunch"),
     path('sensor', sensor, name="sensor"),
-    path('bunchcategory', bunchcategory, name="bunchcategory"),
 ]
