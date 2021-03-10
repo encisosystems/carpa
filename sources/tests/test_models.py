eb9a6cecf265665f757f5e58e11e5b61ac49c95f
David Carranza <dcarranza@hotmail.es>
2021-03-10 15:42:49 -0500

    test vehicle model #177 (#193)
    * Test Bunchbatch model (#183)
    
    * added CRUD tests for BunchBatch model
    
    * added BunchBatch update test
    
    Co-authored-by: rocandres <rcandres@DESKTOP-D7BBAQH.localdomain>
    
    * Create test CRUD for parcel model
    
    * Created unit test of harvester update (#174)
    
    * Test for the Driver model #148 done
    
    * Test for the Driver model #148 (#192)
    
    * Test for the Driver model #148 done
    
    * Update test_models.py
    
    Adjustment to TestDriver class done
    
    * test vehicle model
    
    * Added update test to test vehichle model
    
    Co-authored-by: rocandres <78989283+rocandres@users.noreply.github.com>
    Co-authored-by: rocandres <rcandres@DESKTOP-D7BBAQH.localdomain>
    Co-authored-by: NicolasBorda96 <68128430+NicolasBorda96@users.noreply.github.com>
    Co-authored-by: otoro90 <33351976+otoro90@users.noreply.github.com>
    Co-authored-by: Robinson Fonseca <robinsonfonseca@MacBook-Air-de-Robinson.local>

--- sources/tests/test_models.py
+++ sources/tests/test_models.py
@@ -2,6 +2,70 @@ from django.test import TestCase, tag
 from sources.models import ParcelOwner, Parcel, Driver, Vehicle
 
 
+
+@tag('parcel')
+class TestParcel(TestCase):
+    fixtures = ['test/sources.json']
+
+    def setUp(self):
+        self.queryset = Parcel.objects.all()
+        self.parcel = Parcel.objects.get(pk=10000)
+
+    def test_query_owner(self):
+        owner_status = self.parcel.owner.is_active
+        self.assertEqual(owner_status, True)
+
+    def test_create_parcel(self):
+        new_parcel = Parcel.objects.create(
+            is_active=True,
+            name='acienda las mercedes',
+            owner_id=10000
+        )
+        self.assertEqual(new_parcel.name, 'acienda las mercedes')
+
+    def test_update_parce(self):
+        self.parcel.name='fina buenavista'
+        self.parcel.save()
+        self.assertEqual(self.parcel.name, 'fina buenavista')
+
+    def test_delete_parcel(self):
+        self.parcel.delete()
+        self.assertNotIn(self.parcel, self.queryset)
+
+
+@tag('driver')
+class TestDriver(TestCase):
+    fixtures = ['test/sourcesDriver.json']
+
+    def setUp(self):
+        self.queryset = Driver.objects.all()
+        self.driver = Driver.objects.get(pk=10000)
+
+    def test_query_driver(self):
+        driver_status = self.driver.is_active
+        self.assertEqual(driver_status, True)
+
+    def test_create_driver(self):
+        new_driver = Driver.objects.create(
+            is_active=True,
+            name='Señor X',
+            address='742 Evergreen Terrace',
+            email='amantedelacomida53@aol.com',
+            phone='0',
+            web='compumundohypermegared.net'
+        )
+        self.assertEqual(new_driver.name, 'Señor X')
+
+    def test_update_driver(self):
+        self.driver.name = 'El baron de la cerveza'
+        self.driver.save()
+        self.assertEqual(self.driver.name, 'El baron de la cerveza')
+
+    def test_delete_driver(self):
+        self.driver.delete()
+        self.assertNotIn(self.driver, self.queryset)
+
+
 @tag('vehicles')
 class TestVehicles(TestCase):
     fixtures = ['test/vehicles.json']
@@ -27,3 +91,8 @@ class TestVehicles(TestCase):
     def delete_vehicle(self):
         self.vehicle.delete()
         self.assertNotIn(self.vehicle, self.queryset)
+
+    def test_update_driver(self):
+        self.vehicle.plate = 'MQTT09'
+        self.vehicle.save()
+        self.assertEqual(self.vehicle.plate, 'MQTT09')
