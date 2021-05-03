a80ea29d78013a5adf275c2e927d5c6a3245f049
danialeresbar <daniel.restrepo@unillanos.edu.co>
2021-05-03 14:26:42 -0500

    Fix dependencies issue

--- carpa/settings/development.py
+++ carpa/settings/development.py
@@ -5,7 +5,7 @@ import dj_database_url
 SECRET_KEY = 'p3gm=o9o+_r(5*o$$kn#h*8#n1r)aquf^^nm_v5u0pn^qa$=4*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['*']
 
 
--- carpa/urls.py
+++ carpa/urls.py
@@ -13,7 +13,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-import debug_toolbar
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -27,6 +26,7 @@ urlpatterns = [
 ]
 
 if not settings.IS_PRODUCTION:
+    import debug_toolbar
     urlpatterns += [
         path('__debug__/', include(debug_toolbar.urls)),
     ]
